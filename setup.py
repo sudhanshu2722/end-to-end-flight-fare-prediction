a80f10742a226f6e8c6b8373bd1c226ec89a544e
sudhanshu <bhanuupadhyay302448@gmail.com>
2024-07-22 18:25:33 +0530

    Setup module

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import find_packages,setup
 from typing import List
 
-HYPHEN_E_DOT='-e .'
+HYPHEN_E_DOT='-e.'
 
+# Function to Parse Requirements File
 def get_requirements(file_path:str)->List[str]:
     '''
     This function return list of requirements
@@ -19,7 +20,7 @@ def get_requirements(file_path:str)->List[str]:
 
 setup(
     name='mlproject2',
-    version='0.0.1',
+    version='3.11.5',
     author='Sudhanshu Upadyay',
     author_email='bhanuupadhyay302448@gmail.com',
     packages=find_packages(),
